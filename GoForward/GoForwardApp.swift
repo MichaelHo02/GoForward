70a3e9e489f62dc262b4006907ca78e33bf41f24
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-08-25 19:18:31 +0700

    Initial Commit

---
+++ GoForward/GoForwardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoForwardApp.swift
+//  GoForward
+//
+//  Created by Ho Le Minh Thach on 25/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct GoForwardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
